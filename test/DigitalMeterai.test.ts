7c72e2f0977a6c2d78ec6cffcaf4a386c2927571
stackoverprof <mail.errbint@gmail.com>
2022-10-05 11:49:44 +0700

    feat: transaction testing

--- test/DigitalMeterai.test.ts
+++ test/DigitalMeterai.test.ts
@@ -27,13 +27,37 @@ describe('DigitalMeterai functionality tests', () => {
 		assert.equal(currentValue.toString(), expectedValue.toString());
 	});
 
-	it('Should mint an NFT, and updates appropriately', async () => {
+	it('Should mint several NFT, and updates appropriately', async () => {
 		const txResponse = await DigitalMeterai.mint(24, 1);
 		await txResponse.wait(1);
 
 		const tokenCounter_result = await DigitalMeterai.getTokensTotal();
 		const tokenCounter_expected = 24;
 		assert.equal(tokenCounter_result.toString(), tokenCounter_expected.toString());
+	});
+
+	it('Should change ownership when someone buy the NFT', async () => {
+		const txResponse = await DigitalMeterai.mint(24, ethers.utils.parseEther('4'));
+		await txResponse.wait(1);
+
+		// buyer buy nft
+		const [seller, buyer] = await ethers.getSigners();
+		const sellerInitialBalance = await seller.getBalance();
+
+		const tokenId = 1;
+		const price = await DigitalMeterai.getTokenPrice(tokenId);
+		const txResponse2 = await DigitalMeterai.connect(buyer).buy(tokenId, {
+			value: price,
+		});
+		await txResponse2.wait(1);
+
+		// assert if buyer now owns the token
+		const owner = await DigitalMeterai.ownerOf(tokenId);
+		assert.equal(owner, buyer.address);
+
+		// assert if seller received the payment
+		const sellerFinalBalance = await seller.getBalance();
+		assert.equal(sellerFinalBalance.toString(), sellerInitialBalance.add(price).toString());
 	});
 });
 
