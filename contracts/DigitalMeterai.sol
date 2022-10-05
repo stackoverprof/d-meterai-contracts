8d2b672dd0629a34a697b984888205c4ca518b57
stackoverprof <mail.errbint@gmail.com>
2022-10-05 08:28:28 +0700

    feat: mvp script

--- contracts/DigitalMeterai.sol
+++ contracts/DigitalMeterai.sol
@@ -1,29 +1,105 @@
 // SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 pragma solidity ^0.8.9;
 
-contract DigitalMeterai is ERC721 {
-    uint256 private s_tokenCounter;
+contract DigitalMeterai is ERC721, Ownable {
+    // Events list
+    event DMT___Minted(address _seller, uint256 _price);
+    event DMT___Bought(address _seller, address _buyer, uint256 _price);
+    event DMT___Bound(address _owner, string document);
+
+    // Type declarations
+    enum Status {
+        Available,
+        Paid,
+        Bound
+    }
+
+    // Global variables
+    uint256 private id = 0;
+    string public constant TOKEN_NAME = "Digital Meterai";
+    string public constant TOKEN_SYMBOL = "DMT";
     string public constant TOKEN_URI =
         "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
-    constructor() ERC721("Dogie", "DOG") {
-        s_tokenCounter = 0;
-    }
-    
-    function mintNFT() public returns(uint256){
-        _safeMint(msg.sender, s_tokenCounter);
-        s_tokenCounter = s_tokenCounter + 1;
-        return s_tokenCounter;
+
+    // Each NFT data mapping
+    mapping (uint256 => uint256) public tokenIdToPrice; // Pricing
+    mapping (uint256 => Status) public tokenIdToStatus; // Status
+    mapping (uint256 => string) public tokenIdToDocument; // File Binding
+
+    // Constructor
+    constructor() ERC721(TOKEN_NAME, TOKEN_SYMBOL) {
+        //
     }
 
-    function getTokenCounter() public view returns(uint256){
-        return s_tokenCounter;
+    // Helper functions
+    function getTokensTotal() public view returns(uint256){
+        return id;
     }
 
     function tokenURI(uint256) public view virtual override returns (string memory) {
         return TOKEN_URI;
     }
+
+    
+    // MAIN EXTERNAL FUNCTIONS
+    function mint(uint256 price) external onlyOwner {
+        // Mint new token
+        _safeMint(msg.sender, id);
+
+        // Set initial data
+        tokenIdToPrice[id] = price;
+        tokenIdToStatus[id] = Status.Available;
+        tokenIdToDocument[id] = "";
+        id++;
+
+        // Emit event
+        emit DMT___Minted(msg.sender, price);
+    }
+
+    function buy(uint256 _tokenId) external payable {
+        // Only if never Paid yet
+        Status status = tokenIdToStatus[_tokenId];
+        require(status == Status.Available, "NFT is not available");
+
+        // Only if price is correct
+        uint256 price = tokenIdToPrice[_tokenId];
+        require(msg.value == price, 'Incorrect value');
+        
+        // Transaction transfer
+        address seller = ownerOf(_tokenId);
+        _transfer(seller, msg.sender, _tokenId);
+        payable(seller).transfer(msg.value);
+
+        // Update status
+        tokenIdToStatus[_tokenId] = Status.Paid;
+
+        // Emit event
+        emit DMT___Bought(seller, msg.sender, msg.value);
+    }
+
+    function bind(uint256 _tokenId, string memory _document) external {
+        // Only owner can bind
+        address owner = ownerOf(_tokenId);
+        require(owner == msg.sender, "Only owner can bind");
+
+        // Only if status is Paid
+        Status status = tokenIdToStatus[_tokenId];
+        require(status == Status.Paid, "NFT is not paid yet");
+
+        // Update binding
+        tokenIdToDocument[_tokenId] = _document;
+
+        // Update status
+        tokenIdToStatus[_tokenId] = Status.Bound;
+
+        // Emit event
+        emit DMT___Bound(msg.sender, _document);
+    }
+
+
 }
--- test/DigitalMeterai.test.ts
+++ test/DigitalMeterai.test.ts
@@ -17,27 +17,27 @@ describe('DigitalMeterai functionality tests', () => {
 	it('Initilizes the NFT Correctly.', async () => {
 		const name = await DigitalMeterai.name();
 		const symbol = await DigitalMeterai.symbol();
-		assert.equal(name, 'Dogie');
-		assert.equal(symbol, 'DOG');
+		assert.equal(name, 'Digital Meterai');
+		assert.equal(symbol, 'DMT');
 	});
 
 	it('Should start with initial token counter of 0', async () => {
-		const currentValue = await DigitalMeterai.getTokenCounter();
+		const currentValue = await DigitalMeterai.getTokensTotal();
 		const expectedValue = 0;
 		assert.equal(currentValue.toString(), expectedValue.toString());
 	});
 
 	it('Should mint an NFT, and updates appropriately', async () => {
-		const txResponse = await DigitalMeterai.mintNFT();
+		const txResponse = await DigitalMeterai.mint(1);
 		await txResponse.wait(1);
 
-		const tokenCounter_result = await DigitalMeterai.getTokenCounter();
+		const tokenCounter_result = await DigitalMeterai.getTokensTotal();
 		const tokenCounter_expected = 1;
 		assert.equal(tokenCounter_result.toString(), tokenCounter_expected.toString());
 
-		const tokenURI_result = await DigitalMeterai.tokenURI(0);
-		const tokenURI_expected = await DigitalMeterai.TOKEN_URI();
-		assert.equal(tokenURI_result, tokenURI_expected);
+		// const tokenURI_result = await DigitalMeterai.tokenURI(0);
+		// const tokenURI_expected = await DigitalMeterai.TOKEN_URI();
+		// assert.equal(tokenURI_result, tokenURI_expected);
 	});
 });
 
