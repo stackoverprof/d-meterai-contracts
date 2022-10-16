a53f59494e4708937135756e88039a9cbb5fa447
stackoverprof <mail.errbint@gmail.com>
2022-10-16 18:40:37 +0700

    feat: add npm scripts and env example

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,7 +7,6 @@ import 'hardhat-abi-exporter';
 import 'solidity-coverage';
 import 'dotenv/config';
 
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || '';
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || '';
 const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || '';
@@ -21,11 +20,6 @@ const config: HardhatUserConfig = {
 		localhost: {
 			url: 'http://127.0.0.1:8545/',
 			chainId: 31337,
-		},
-		rinkeby: {
-			url: RINKEBY_RPC_URL,
-			chainId: 4,
-			accounts: [PRIVATE_KEY],
 		},
 		goerli: {
 			url: GOERLI_RPC_URL,
