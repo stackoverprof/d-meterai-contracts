6ee9cba4c33de0e3ee331976ff47743bcbeb4be2
stackoverprof <mail.errbint@gmail.com>
2022-10-16 18:41:38 +0700

    docs: explain

--- utils/verify.ts
+++ utils/verify.ts
@@ -1,14 +1,15 @@
 import { run } from 'hardhat';
 
 export const verify = async (contractAddress: string, args: any[]) => {
-	console.log('Verifying contract...');
-
+	// Start the process of verifying the contract
 	await run('verify:verify', {
 		address: contractAddress,
 		constructorArguments: args,
 	}).catch((err: any) => {
+		// If the contract is already verified, we don't need to do anything
 		if (err.message.toLowerCase().includes('already verified'))
 			console.log('Contract already verified');
+		// Display the error message if verification fails
 		else console.error('error verifying:', err);
 	});
 };
